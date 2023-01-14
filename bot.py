856fb9679b0f73288dc6c0c02c64e39f4bb5fdf2
Jpizza99 <Jasonpitchford@gmail.com>
2023-01-14 17:00:28 -0500

    more img + some fixes

--- bot.py
+++ bot.py
@@ -1,12 +1,21 @@
 import tweepy
+from tweepy import OAuthHandler
 import requests
 import time
 import random
 
 
+IMAGE_COUNT = 32
+
+
+
 
 #Quotable Setup
 QUOTABLE_URL = 'https://api.quotable.io/random'
+x = []
+
+for i in range(1, IMAGE_COUNT+1):
+  x.append(i) 
 
 while True:
   QuoteJSON = requests.get(QUOTABLE_URL).json()
@@ -16,9 +25,18 @@ while True:
   print(quote)
 
   
-  auth = tweepy.OAuth1UserHandler(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+  auth = OAuthHandler(API_KEY, API_KEY_SECRET)
+  auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
   api = tweepy.API(auth)
-  media = api.media_upload('images\\BG'+str(random.randint(1,16)) + '.png')
+
+
+  if(len(x) != 0):
+    image = x.pop(random.randint(0,len(x)-1))
+  else:
+    for i in range(1, IMAGE_COUNT+1):
+      x.append(i) 
+  
+  media = api.media_upload('images/BG'+str(image) + '.png')
 
   api.update_status(quote, media_ids=[media.media_id])
   time.sleep(1500)
